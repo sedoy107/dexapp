695d4d0d0fbcc5ee24aff5d6d1b4564c6b0a95b9
sgorbov <noname@FC008866.local>
2021-12-13 01:54:36 -0600

    Add STRAT_BLOCK constant

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -6,3 +6,10 @@ export const LIMIT = 1
 export const IOC = 2
 export const FOK = 3
 export const MOC = 4
+
+export const WITHDRAW = 0
+export const DEPOSIT = 1
+export const MINT = 2
+export const BURN = 3
+
+export const START_BLOCK = 28731619
